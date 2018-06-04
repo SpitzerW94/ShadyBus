f267f8fa7e44121a0545a484cc836f9fccc67a43
Will Spitzer <will.spitzer94@googlemail.com>
2018-06-04 14:09:55 +0100

    Committed the local changes to the repo
    Committed the local changes to the repo

---
+++ app/src/main/java/com/time/table/TabFragment1.java
@@ -0,0 +1,24 @@
+package com.time.table;
+/*
+ * Created by will.spitzer on 01/06/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+public class TabFragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_fragment_1, container, false);
+
+        WebView webView = (WebView) view.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.trentbarton.co.uk/bus-information/live-bus-departures/?stop=16311");
+
+        return view;
+    }
+}
